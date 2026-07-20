e4a46f2c2ece2def868a9c9181996ad96a6fa4ae
cakihatice <cakihatice@gmail.com>
2026-07-20 10:26:25 +0300

    Make Description field nullable

--- Domain/Entities/TodoItem.cs
+++ Domain/Entities/TodoItem.cs
@@ -4,7 +4,7 @@ namespace TodoApi.Domain.Entities
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public string? Description { get; set; } = string.Empty;
+        public string? Description { get; set; }
         public bool IsCompleted { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;    
     }
